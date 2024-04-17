a4bf6f7397477e25b7c553173d78e8e30f1dff80
Raphael Silva Ramalho <raphaelsilvaramalho.com@gmail.com>
2024-04-17 09:35:47 -0300

    adicionando painel de noticias

--- src/components/NewsHome.jsx
+++ src/components/NewsHome.jsx
@@ -1,6 +1,20 @@
 import React from "react";
 
 const NewsHome = () => {
+  const urls = [
+    "https://s2-g1.glbimg.com/Mu9tTg4bj7LB4jvzf70fHVJb7xw=/0x0:2000x1334/1008x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_59edd422c0c84a879bd37670ae4f538a/internal_photos/bs/2024/P/d/SdmuaPShCFBphvdkcsaQ/juze-lukele-no-rancho-fundo.jpg",
+    "https://s2-g1.glbimg.com/M1sQesX9WmmN1KqbwXpDTIiHNRM=/0x0:683x421/924x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_59edd422c0c84a879bd37670ae4f538a/internal_photos/bs/2024/B/9/ullUrVQDun3JyMS0UjDQ/zecapagodinhodjonga.jpg",
+    "https://s2-g1.glbimg.com/yiF0S6Rl4hM3dMxxeEmMVug0h0c=/0x0:1280x853/984x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_59edd422c0c84a879bd37670ae4f538a/internal_photos/bs/2019/U/l/KAv5VjSbCUVMK8Aos8uA/whatsapp-image-2019-05-04-at-07.23.16-1-.jpeg",
+    "https://s2-g1.glbimg.com/mlWRnnRc-o7ywLSQFUArvDUTeas=/0x0:1402x1402/1008x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_59edd422c0c84a879bd37670ae4f538a/internal_photos/bs/2024/s/4/YPawfwTlqZKWmM5dLWaQ/ferrugem-steff-lima.jpg",
+  ];
+
+  const manchetes = [
+    "Juzé & Lukete vão do aboio à bossa em EP lançado no embalo do retorno dos cantadores à novela ‘No rancho fundo’",
+    "Zeca Pagodinho volta a saudar Ogum com versos de Jorge Ben Jor em gravação ao vivo com o rapper Djonga",
+    "Lauryn Hill fará show em SP para comemorar 50 anos do famoso baile de black music Chic Show",
+    "Ferrugem reúne a Turma do Pagode no álbum em que festeja dez anos na roda",
+  ];
+
   return (
     <div>
       <h1
@@ -9,7 +23,34 @@ const NewsHome = () => {
       >
         Oque rola no mundo da música?
       </h1>
-      news
+
+      <div className="grid grid-cols-2 gap-36 justify-items-center mt-10">
+        {urls.map((url, index) => {
+          return (
+            <div
+              style={{
+                backgroundColor: "#B37AC5",
+                width: "100%",
+                height: "500px",
+              }}
+            >
+              <img
+                src={url}
+                alt=""
+                style={{ width: "100%", height: "100%", objectFit: "cover" }}
+              />
+              <p
+                className="text-2xl py-5 text-white font-bold px-10 border border-black"
+                style={{
+                  backgroundColor: "#B37AC5",
+                }}
+              >
+                {manchetes[index]}
+              </p>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 };
