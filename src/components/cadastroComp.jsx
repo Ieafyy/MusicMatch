2bff6de6935c449f5f0d581505fa4949a03fbf6b
Raphael Silva Ramalho <raphaelsilvaramalho.com@gmail.com>
2024-04-23 21:25:24 -0300

    tela de login e cadastro

--- src/components/cadastroComp.jsx
+++ src/components/cadastroComp.jsx
@@ -3,11 +3,20 @@ import InputField from "./InputField";
 import { motion } from "framer-motion";
 
 const CadastroComp = ({ setState }) => {
+  const cardStyle = {
+    background: "rgba( 132, 28, 148, 0.45 )",
+    boxShadow: "0 8px 32px 0 rgba(31, 38, 135, 0.37)",
+    backdropFilter: "blur(5px)",
+    WebkitBackdropFilter: "blur(5px)",
+    borderRadius: "10px",
+  };
+
   return (
     <div>
       <div className="grid place-content-center">
         <motion.div
-          className="bg-cinza p-20 flex flex-col justify-center rounded-xl mt-20"
+          style={cardStyle}
+          className="p-20 flex flex-col justify-center rounded-xl my-20"
           animate={{
             opacity: [0, 1],
             translateY: [500, 0],
@@ -23,13 +32,59 @@ const CadastroComp = ({ setState }) => {
           >
             Cadastro
           </h1>
-          <InputField placeholder="User" type="text" icon="fas fa-user" />
-          <InputField placeholder="Senha" type="password" icon="fas fa-lock" />
-          <button className="bg-roxo text-white py-3 px-20 rounded-xl mt-10 text-xl font-bold place-self-center hover:bg-gray-200">
-            Login
+          <div className="flex flex-col gap-0">
+            <p className="text-4xl font-bold">Sou: </p>
+            <div className="text-2xl mt-3">
+              <input
+                type="radio"
+                name="tipo"
+                id="radioOuv"
+                className="scale-125 mr-1"
+              />
+              <label htmlFor="radioOuv" className="mr-10">
+                Ouvinte
+              </label>
+              <input
+                type="radio"
+                name="tipo"
+                id="radioMus"
+                className="scale-125 mr-1"
+              />
+              <label htmlFor="radioMus">Músico</label>
+            </div>
+            <p className="text-4xl font-bold mb-3 mt-8">Nome de usuário:</p>
+            <InputField placeholder="User" type="text" icon="fas fa-user" />
+
+            <p className="text-4xl font-bold mb-3 mt-8">Email:</p>
+            <InputField
+              placeholder="Email"
+              type="email"
+              icon="fas fa-envelope"
+            />
+
+            <p className="text-4xl font-bold mb-3 mt-8">Senha:</p>
+            <div className="flex gap-10">
+              <InputField
+                placeholder="Senha"
+                type="password"
+                icon="fas fa-lock"
+              />
+              <InputField
+                placeholder="Repetir a senha"
+                type="password"
+                icon="fas fa-lock"
+              />
+            </div>
+
+            <p className="text-4xl font-bold mb-3 mt-8">Data de nascimento:</p>
+            <InputField type="date" icon="fas fa-calendar" />
+          </div>
+
+          <button className="bg-roxo duration-300 text-white py-3 px-20 rounded-xl mt-10 text-xl font-bold place-self-center hover:bg-gray-100 hover:text-roxo">
+            Cadastrar
           </button>
           <p
-            className="text-xl underline mt-4 text-white cursor-pointer"
+            className="text-xl underline mt-4 cursor-pointer text-center"
             onClick={() => setState("login")}
           >
             Já possui conta? Faça seu login aqui!
--- src/components/loginComp.jsx
+++ src/components/loginComp.jsx
@@ -3,11 +3,20 @@ import InputField from "./InputField";
 import { motion } from "framer-motion";
 
 const LoginComp = ({ setState }) => {
+  const cardStyle = {
+    background: "rgba( 132, 28, 148, 0.45 )",
+    boxShadow: "0 8px 32px 0 rgba(31, 38, 135, 0.37)",
+    backdropFilter: "blur(5px)",
+    WebkitBackdropFilter: "blur(5px)",
+    borderRadius: "10px",
+  };
+
   return (
     <div>
       <div className="grid place-content-center">
         <motion.div
-          className="bg-cinza p-20 flex flex-col justify-center rounded-xl mt-20"
+          style={cardStyle}
+          className="p-20 flex flex-col justify-center rounded-xl mt-20"
           animate={{
             opacity: [0, 1],
             translateY: [500, 0],
@@ -25,11 +34,11 @@ const LoginComp = ({ setState }) => {
           </h1>
           <InputField placeholder="User" type="text" icon="fas fa-user" />
           <InputField placeholder="Senha" type="password" icon="fas fa-lock" />
-          <button className="bg-roxo text-white py-3 px-20 rounded-xl mt-10 text-xl font-bold place-self-center hover:bg-gray-200">
+          <button className="bg-roxo duration-300 text-white py-3 px-20 rounded-xl mt-10 text-xl font-bold place-self-center hover:bg-gray-100 hover:text-roxo">
             Login
           </button>
           <p
-            className="text-xl underline mt-4 text-white cursor-pointer"
+            className="text-xl underline mt-4 cursor-pointer"
             onClick={() => setState("cadastro")}
           >
             Ou se cadastre em nossa plataforma!
