d250fa0af9296807efd8a5f255c2a38b40f6545f
Raphael Silva Ramalho <raphaelsilvaramalho.com@gmail.com>
2024-04-26 09:54:16 -0300

    estaTICOOOOOO

--- arquivos estaticos/assets/navigation.js
+++ arquivos estaticos/assets/navigation.js
@@ -6,6 +6,14 @@ function handleClickNavigation(action) {
   const selectorMusicaDesk = $("#selectorMusicaDesk");
   const selectorArtistaDesk = $("#selectorArtistaDesk");
   const selectorGeneroDesk = $("#selectorGeneroDesk");
+  //------------------------------------------------------
+  const navArtistaMobile = $("#nav-artista-mobile");
+  const navMusicaMobile = $("#nav-musica-mobile");
+  const navGeneroMobile = $("#nav-genero-mobile");
+
+  const selectorMusicaMobile = $("#selectorMusicaMobile");
+  const selectorArtistaMobile = $("#selectorArtistaMobile");
+  const selectorGeneroMobile = $("#selectorGeneroMobile");
 
   switch (action) {
     case "musica-desk":
@@ -36,6 +44,36 @@ function handleClickNavigation(action) {
       selectorGeneroDesk.removeClass("bg-black").addClass("bg-roxo w-64");
       selectorMusicaDesk.removeClass("bg-roxo w-64").addClass("bg-black");
       selectorArtistaDesk.removeClass("bg-roxo w-64").addClass("bg-black");
+      break;
+
+    case "musica-mobile":
+      navMusicaMobile.removeClass("hidden").addClass("flex");
+      navArtistaMobile.removeClass("flex").addClass("hidden");
+      navGeneroMobile.removeClass("flex").addClass("hidden");
+      //------------------------------------------------
+      selectorMusicaMobile.removeClass("bg-black").addClass("bg-roxo w-64");
+      selectorArtistaMobile.removeClass("bg-roxo w-64").addClass("bg-black");
+      selectorGeneroMobile.removeClass("bg-roxo w-64").addClass("bg-black");
+      break;
+
+    case "artista-mobile":
+      navArtistaMobile.removeClass("hidden").addClass("flex");
+      navMusicaMobile.removeClass("flex").addClass("hidden");
+      navGeneroMobile.removeClass("flex").addClass("hidden");
+      //------------------------------------------------
+      selectorArtistaMobile.removeClass("bg-black").addClass("bg-roxo w-64");
+      selectorMusicaMobile.removeClass("bg-roxo w-64").addClass("bg-black");
+      selectorGeneroMobile.removeClass("bg-roxo w-64").addClass("bg-black");
+      break;
+
+    case "genero-mobile":
+      navGeneroMobile.removeClass("hidden").addClass("flex");
+      navMusicaMobile.removeClass("flex").addClass("hidden");
+      navArtistaMobile.removeClass("flex").addClass("hidden");
+      //------------------------------------------------
+      selectorGeneroMobile.removeClass("bg-black").addClass("bg-roxo w-64");
+      selectorMusicaMobile.removeClass("bg-roxo w-64").addClass("bg-black");
+      selectorArtistaMobile.removeClass("bg-roxo w-64").addClass("bg-black");
       break;
   }
 }
--- arquivos estaticos/assets/swiper.js
+++ arquivos estaticos/assets/swiper.js
@@ -1,6 +1,10 @@
 console.log("init");
 
 const swiper = new Swiper(".swiper", {
+  spaceBetween: 100,
+  autoplay: {
+    delay: 2000,
+  },
   navigation: {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
