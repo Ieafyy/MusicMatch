0709972b4ba70449e09c21109af2987cc4b7ef30
Raphael Silva Ramalho <raphaelsilvaramalho.com@gmail.com>
2024-05-05 16:56:37 -0300

    nova pagina

--- arquivos estaticos/assets/cadastroNav.js
+++ arquivos estaticos/assets/cadastroNav.js
@@ -2,8 +2,10 @@ function toggle(content) {
   if (content == "login") {
     $("#loginCard").removeClass("hidden").addClass("block");
     $("#cadastroCard").removeClass("block").addClass("hidden");
+    document.title = "login";
   } else if (content == "cadastro") {
     $("#cadastroCard").removeClass("hidden").addClass("block");
     $("#loginCard").removeClass("block").addClass("hidden");
+    document.title = "cadastro";
   }
 }
